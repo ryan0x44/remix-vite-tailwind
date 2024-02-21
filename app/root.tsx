fc59cfdf8c1435581a2aa454705ab271d156b3f2
Ryan D <git@ryan0x44.com>
2024-02-21 21:59:38 +1100

    Tailwind config per https://remix.run/docs/en/main/styling/tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,13 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/cloudflare";
+
+import styles from "./tailwind.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
